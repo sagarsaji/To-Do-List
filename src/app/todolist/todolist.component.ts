90b33a79a2ea7801a82f02ebc74ba33d62509cf3
Sagar Saji <sagarsaji7@gmail.com>
2023-06-08 11:37:44 +0530

    todolist

---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+  taskArray = [{ taskName: 'Brush teeth', isCompleted: false, isEditing: false }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false,
+      isEditing: false
+    })
+
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    console.log(index);
+
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number) {
+    console.log(this.taskArray);
+
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+
+  onUpdate(index: number){
+    const task = this.taskArray[index];
+    task.isEditing = false;
+
+  // Perform any additional logic or updates here
+    console.log('Task updated:', task.taskName);
+  }
+
+}
